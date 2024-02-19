8725debb2841afededb4888e445186474c3a77cd
Aleksei Koshkin <koshkin.pydev@gmail.com>
2024-02-19 19:38:26 +0300

    Add linters and logging

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-from fastapi import FastAPI
+import logging
+from fastapi import FastAPI, status
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="commutation.log",
+    filemode="w",
+    format="%(asctime)s %(name)s %(levelname)s:%(message)s",
+)
+
+logger = logging.getLogger(__name__)
+consoleHandler = logging.StreamHandler()
+logFormatter = logging.Formatter(
+    "%(levelname)s: %(asctime)s | %(threadName)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+consoleHandler.setFormatter(logFormatter)
+logger.addHandler(consoleHandler)
 
 app = FastAPI()
 
 
-@app.get("/status")
+@app.get("/status/", status_code=status.HTTP_200_OK)
 async def healthcheck():
+    logger.info("Healthcheck")
     return {"status": "ok"}
-
